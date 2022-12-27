291a99694d47ae9ddd01ab6c2f590b05fe5836e6
lysvm <lysenkovm@yandexlyceum.ru>
2022-12-27 17:26:59 +0600

    Плавная змейка - кривая

--- pithon.py
+++ pithon.py
@@ -1,12 +1,21 @@
 import pygame
 import random
 from itertools import product
+from operator import itemgetter
 
 
 
+# ['left', 'up', 'right', 'down']
 DIRECTIONS = ['left', 'up', 'right', 'down']
-DIR_KEYS = dict(zip((pygame.K_LEFT, pygame.K_UP, pygame.K_RIGHT,
+# {1073741904: 'left', 1073741906: 'up', 1073741903: 'right', 1073741905: 'down'}
+KEYS_DIRS = dict(zip((pygame.K_LEFT, pygame.K_UP, pygame.K_RIGHT,
                      pygame.K_DOWN), DIRECTIONS))
+# {'left': 1073741904, 'up': 1073741906, 'right': 1073741903, 'down': 1073741905}
+DIRS_KEYS = dict(zip(KEYS_DIRS.values(), KEYS_DIRS.keys()))
+# {'left': 'right', 'up': 'down', 'right': 'left', 'down': 'up'}
+OPPO_DIRS = dict(zip(DIRECTIONS, itemgetter(2, 3, 0, 1)(DIRECTIONS)))
+# {1073741904: 1073741903, 1073741906: 1073741905, 1073741903: 1073741904, 1073741905: 1073741906}
+OPPO_KEYS = {DIRS_KEYS[d]: DIRS_KEYS[op_d] for d, op_d in OPPO_DIRS.items()}
 # {'strait': {'left': (0, -1), 'right': (0, 1), 'up': (1, -1), 'down': (1, 1)},
 # 'back': {'left': (0, 1), 'right': (0, -1), 'up': (1, 1), 'down': (1, -1)}}
 PLUS_MINUS = {'strait': dict(zip(('left', 'right', 'up', 'down'),
@@ -15,11 +24,11 @@ PLUS_MINUS = {'strait': dict(zip(('left', 'right', 'up', 'down'),
                                product((0, 1), (1, -1))))}
 
 
-def square_plus_minus(square, value, direction, strait_or_back):
-    square = list(square)
+def coords_plus_minus(coords, value, direction, strait_or_back):
+    coords = list(coords)
     idx, sign = PLUS_MINUS[strait_or_back][direction]
-    square[idx] += value * sign
-    return tuple(square)
+    coords[idx] += value * sign
+    return tuple(coords)
 
 
 class Game:
@@ -29,7 +38,7 @@ class Game:
                           self.square_size
 
         # Создать объект Змеи
-        self.snake = Snake(self, 15)
+        self.snake = Snake(self, self.square_size * 2)
         # Создать группу спрайтов Яблок
         # self.apples = pygame.sprite.Group()
 
@@ -50,33 +59,37 @@ class Snake(pygame.sprite.Group):
 
         self.dir = random.choice(DIRECTIONS)
         
-        # Добавить первую часть: и хвост, и голова
+        # Добавить первую часть - и хвост, и голова
         ## Создать головную ячейку
-        head_square = (self.game.field_size[0] // 2,
-                       self.game.field_size[1] // 2)
-        head_cell = Snake_cell(head_square)
+        head_coords = (self.game.field_size[0] // 2 * self.game.square_size,
+                       self.game.field_size[1] // 2 * self.game.square_size)
+        head_cells = [Snake_cell(coords_plus_minus(head_coords, 1, self.dir,
+                                                   'back'), visual_head=True)
+                      for i in range(self.game.square_size)]
         ## Создать головную часть с головной ячейкой
-        snake_head = Snake_part(groups=[self], snake=self, length=length, dir=self.dir, cells=[head_cell])
-        for i in range(1, length):
-            square = square_plus_minus(head_square, i, snake_head.dir, 'back')
-            snake_head.cells.insert(0, Snake_cell(square))
+        snake_head = Snake_part(groups=[self], snake=self, length=length,
+                                dir=self.dir, cells=head_cells)
+        for i in range(len(head_cells), length):
+            coords = coords_plus_minus(head_cells[-1].coords, i,
+                                       snake_head.dir, 'back')
+            snake_head.insert(Snake_cell(coords))
         snake_head.renew_image_rect()
 
     def get_head(self, item='part'):
         head_part = self.sprites()[-1]
         head_cell = head_part.cells[-1]
-        head_square = head_cell.square
+        head_coords = head_cell.coords
         head_items = {'part': head_part,
                       'cell': head_cell,
-                      'square': head_square}
+                      'coords': head_coords}
         return head_items[item]
     
     def update(self):
         parts = self.sprites()
-        last_cells = [part.get_last_cell() for part in parts]  # Выделить последние ячейки из всех частей
+        last_cells = [part.pop() for part in parts]  # Выделить последние ячейки из всех частей
         head_cell = last_cells.pop(-1)  # Выделить головную ячейку в отдельную переменную
         if last_cells:  # Переставить передние ячейки в следующие части
-            [parts[i + 1].insert_cell(last_cells[i]) for i in range(len(last_cells))]
+            [parts[i + 1].insert(last_cells[i]) for i in range(len(last_cells))]
         if not parts[0].cells:  # Если хвостовая часть не содержит ячеек - удалить ее из группы
             parts[0].remove(self)
         # Создать новую головную часть
@@ -84,7 +97,7 @@ class Snake(pygame.sprite.Group):
             new_head_part = Snake_part(groups=[self], snake=self, length=1,
                                        dir=self.dir, cells=[head_cell])
         else:
-            self.get_head('part').insert_cell(head_cell, -1)
+            self.get_head('part').insert(head_cell, -1)
         # Вызвать методы update у каждого спрайта (части)
         parts = self.sprites()
         for part in parts:
@@ -101,13 +114,14 @@ class Snake_part(pygame.sprite.Sprite):
         # snake: Snake
         # length: int
         # dir: str
-        # cells: [Snake_cell]
+        # cells: [Snake_cells]
         super().__init__(*kwargs['groups'])
         self.length = kwargs['length'] if 'length' in kwargs else 1
         self.snake = kwargs['snake']
         self.dir = kwargs['dir'] if 'dir' in kwargs else self.snake.dir
         # Добавляем все ячейки в новую часть (головную)
-        self.cells = kwargs['cells'] if 'cells' in kwargs else []
+        self.cells = [cell for cell in kwargs['cells']]
+        [cell.update(part=self) for cell in self.cells]
 
 
     def index(self):
@@ -126,30 +140,34 @@ class Snake_part(pygame.sprite.Sprite):
         x1, y1, x2, y2 = self.get_sprite_coords()
         return pygame.Surface((x2 - x1, y2 - y1))
 
-    # Используется метод 'square_plus_minus'
+    # Используется метод 'coords_plus_minus'
     def get_rect(self):
         x1, y1, x2, y2 = self.get_sprite_coords()
         return pygame.Rect(x1, y1, x2 - x1, y2 - y1)
 
     def get_sprite_coords(self):
-        hor_coords = tuple(map(lambda cell: cell.square[0], self.cells))
+        hor, ver = self.cells[0].get_sizes()
+        print(hor, ver)
+        hor_coords = tuple(map(lambda cell: cell.coords[0], self.cells))
         left = min(hor_coords)
         right = max(hor_coords)
-        ver_coords = tuple(map(lambda cell: cell.square[1], self.cells))
+        ver_coords = tuple(map(lambda cell: cell.coords[1], self.cells))
         up = min(ver_coords)
         down = max(ver_coords)
-        x1 = (left - 1) * self.snake.game.square_size
-        y1 = (up - 1) * self.snake.game.square_size
-        x2 = (right) * self.snake.game.square_size
-        y2 = (down) * self.snake.game.square_size
+        x1 = left - hor
+        y1 = up - ver
+        x2 = right
+        y2 = down
         return (x1, y1, x2, y2)
 
     def update(self):
         for cell in self.cells:  # передвинуть вперед каждую ячейку в части
-            cell.update(square_plus_minus(cell.square, 1, self.dir, 'strait'))
+            cell.update(coords=coords_plus_minus(cell.coords, 1,
+                                                 self.dir, 'strait'))
         self.renew_image_rect()
 
-    def insert_cell(self, cell, idx=0):
+    def insert(self, cell, idx=0):
+        cell.update(part=self)
         if idx == -1:
             self.cells.append(cell)
         else:
@@ -158,17 +176,33 @@ class Snake_part(pygame.sprite.Sprite):
     def get_length(self):
         return len(self.cells)
 
-    def get_last_cell(self):
-        return self.cells.pop(-1)
+    def pop(self, index=-1):
+        return self.cells.pop(index)
+
 
 
 class Snake_cell:
-    def __init__(self, square):
-        self.square = square
-        
-    def update(self, square):
-        self.square = square
-        
+    def __init__(self, coords, part=False, visual_head=False):
+        self.coords = coords
+        self.part = part
+        self.visual_head = visual_head
+
+    def update(self, **kwargs):
+        if 'coords' in kwargs:
+            self.coords = kwargs['coords']
+        if 'part' in kwargs:
+            self.part = kwargs['part']
+        if 'visual_head' in kwargs:
+            self.visual_head = kwargs['visual_head']
+
+    def get_sizes(self):
+        if self.part.dir in ('up', 'down'):
+            hor = self.part.snake.game.square_size
+            ver = 1
+        elif self.part.dir in ('left', 'right'):
+            hor = 1
+            ver = self.part.snake.game.square_size
+        return (hor, ver)
 
         
 class Apple(pygame.sprite.Sprite):
@@ -194,11 +228,14 @@ if __name__ == '__main__':
     size = 1280, 1024
     screen = pygame.display.set_mode(size)
     screen.fill('white')
-    fps = 10
+    fps = 50
     clock = pygame.time.Clock()
 
     game = Game()
-    
+
+
+    test = 0
+    new_snake_dir = False
     running = True
     while running:
         for event in pygame.event.get():
@@ -207,12 +244,15 @@ if __name__ == '__main__':
                 break
 
             if event.type == pygame.KEYDOWN:
-                if event.key in (pygame.K_UP, pygame.K_DOWN) and \
-                   game.snake.dir not in ('up', 'down'):
-                    game.snake.change_dir(DIR_KEYS[event.key])
-                elif event.key in (pygame.K_LEFT, pygame.K_RIGHT) and \
-                     game.snake.dir not in ('left', 'right'):
-                    game.snake.change_dir(DIR_KEYS[event.key])
+                print(event.key, KEYS_DIRS[event.key])
+                if (event.key in KEYS_DIRS) and (game.snake.dir not in \
+                        (KEYS_DIRS[event.key], KEYS_DIRS[OPPO_KEYS
+                        [event.key]])):
+                    new_snake_dir = KEYS_DIRS[event.key]
+        if all(map(lambda coord: not coord % game.square_size,
+                   game.snake.get_head('coords'))) and new_snake_dir:
+            game.snake.change_dir(new_snake_dir)
+            new_snake_dir = False
             
         screen.fill('white')
         game.snake.draw(screen)
